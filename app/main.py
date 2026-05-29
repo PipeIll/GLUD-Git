c1e0a5cd054cc1afc01972f4b5ad9f6728799740
Sebasitian Garzon <Sebastian>
2026-05-29 11:17:32 -0500

    feat: implementacion de frontend sencillo para visualizar clientes y rutinas

--- app/main.py
+++ app/main.py
@@ -2,12 +2,16 @@ from fastapi import FastAPI
 from app.routes.clients import router as clients_router
 from app.routes.routines import router as routines_router
 from app.routes.progress import router as progress_router
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
+
 
 app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "API running"}
+    return RedirectResponse(url="/frontend/index.html")
 
 @app.get("/health")
 def health():
@@ -16,3 +20,13 @@ def health():
 app.include_router(clients_router)
 app.include_router(routines_router)
 app.include_router(progress_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.mount("/frontend", StaticFiles(directory="frontend"), name="frontend")
